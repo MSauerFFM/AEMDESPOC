9318cbddde0e364920f0135fa51165c80ac4396d
Marco Sauer <marco.sauer@trueinnovation.de>
2026-03-25 15:15:42 +0100

    Change inner text to miau

--- blocks/miau/miau.js
+++ blocks/miau/miau.js
@@ -2,6 +2,7 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     [...row.children].forEach((div) => {
 		div.className = 'miau';
+		div.innerHTML  = "MIAU"
     });
   });
 }
