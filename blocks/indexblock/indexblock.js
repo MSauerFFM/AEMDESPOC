24a206b7c487b9bd230cd29db63f08ddfd7f037a
Marco Sauer <marco.sauer@trueinnovation.de>
2026-03-26 14:04:14 +0100

    indexblock component

---
+++ blocks/indexblock/indexblock.js
@@ -0,0 +1,27 @@
+export default async function decorate(block) {
+    const config = readBlockConfig(block);
+	const indexUrl = config.pathtoindex;
+	
+	// Fetch data from the API.
+    const response = await fetch(indexUrl);
+    if (!response.ok) {
+      throw new Error(`API returned ${response.status}`);
+    }
+    const data = await response.json();
+	
+	const container = document.createElement('div');
+	data.forEach( (item) =>
+		const titleDiv = document.createElement('div');
+		titleDiv.textContent = '${item.title}';
+		container.appendChild(titleDiv);
+		
+		const descriptionDiv = document.createElement('div');
+		descriptionDiv.textContent = '${item.description}';
+		container.appendChild(descriptionDiv);
+		
+		const picture = document.createElement('img');
+		picture.src = '{item.image}';
+		container.appendChild(picture);
+	)
+	block.appendChild(container);
+}
